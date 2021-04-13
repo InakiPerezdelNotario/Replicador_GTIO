54e8cf9eb5298b7da3c8e600da56ee065cfb6429
root <root@eim-alu-69090.lab.unavarra.es>
2021-04-13 13:14:45 +0200

    He modificado la ip del inicializador de los servicios de kong

--- inicializar.sh
+++ inicializar.sh
@@ -1,7 +1,7 @@
 curl -i -X POST \
   --url http://172.18.69.90:8001/services/ \
   --data 'name=replicador' \
-  --data 'url=http://172.25.0.3:8080'
+  --data 'url=http://192.168.64.3:8080'
   
 curl -i -X POST \
   --url http://172.18.69.90:8001/services/replicador/routes \
