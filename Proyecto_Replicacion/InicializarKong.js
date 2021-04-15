14058354cce60ed2986a5d873bad94c001785b8b
root <root@eim-alu-69090.lab.unavarra.es>
2021-04-15 10:15:07 +0200

    He quitado los returns que habia in InicializarKong.js para que no ser salga antes de terminar la inicializacion

--- Proyecto_Replicacion/InicializarKong.js
+++ Proyecto_Replicacion/InicializarKong.js
@@ -24,20 +24,17 @@ for (const name of Object.keys(nets)) {
   --data "name=replicador" \
   --data "url=http://' + dir_ip + ':8080"', (error, stdout, stderr) => {
 	if (error) {
-		console.log(`error: ${error.message}`);
-		console.log('ERROR0');
-		return error;
+		//console.log(`error: ${error.message}`);
+		//console.log('ERROR0');
 	}
 
 	else if (stderr) {
-		console.log(`stderr: ${stderr}`);
-		console.log('ERROR1');
-		return stderr;
+		//console.log(`stderr: ${stderr}`);
+		//console.log('ERROR1');
 	}
 	else{
-		console.log(`stdout:\n${stdout}`);
-		console.log('TERMINADO');
-		return stdout;
+		//console.log(`stdout:\n${stdout}`);
+		//console.log('TERMINADO');
 	}
   });
 
@@ -46,19 +43,16 @@ exec('curl -i -X POST \
   --url http://172.18.69.90:8001/services/replicador/routes \
   --data "hosts[]=replicador.com"', (error, stdout, stderr) => {
 	if (error) {
-		console.log(`error: ${error.message}`);
-		console.log('ERROR0');
-		return error;
+		//console.log(`error: ${error.message}`);
+		//console.log('ERROR0');
 	}
 
 	else if (stderr) {
-		console.log(`stderr: ${stderr}`);
-		console.log('ERROR1');
-		return stderr;
+		//console.log(`stderr: ${stderr}`);
+		//console.log('ERROR1');
 	}
 	else{
-		console.log(`stdout:\n${stdout}`);
-		console.log('TERMINADO');
-		return stdout;
+		//console.log(`stdout:\n${stdout}`);
+		//console.log('TERMINADO');
 	}
   });
